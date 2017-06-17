262f048efc03d5e36e268a1f33a8d61e28df7ffd
Chad Phillips <chad@apartmentlines.com>
2017-06-16 19:40:56 -0700

    update video banner colors

--- video/site.js
+++ video/site.js
@@ -3,8 +3,8 @@ var config = {};
 config.video_mute_image = 'facilitateanywhere-conference-video-mute.png';
 config.video_no_video_avatar_image = 'facilitateanywhere-conference-video-no-video-avatar.png';
 config.video_banner_text_color = '#FFFFFF';
-config.video_banner_background = '#003366';
-config.video_mute_banner_text_color = '#003366';
+config.video_banner_background = '#000000';
+config.video_mute_banner_text_color = '#0d47a1';
 config.video_mute_banner_background = '#FFFFFF';
 
 module.exports.config = config;
