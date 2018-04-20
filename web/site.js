4fd7c2e5de5cb47d7a325fb647bb0e6f88971a42
Chad Phillips <chad@apartmentlines.com>
2018-04-19 20:18:41 -0700

    rip out alt sessions

--- web/site.js
+++ web/site.js
@@ -26,32 +26,6 @@ var db = {};
 db.events = [];
 
 db.sessions = [];
-db.sessions.push({
-	id: 1,
-	title: "Alternate breakout #1",
-	description: "Click the button below to join the breakout",
-	shortCode: "alt-session-1",
-	joinCap: 10,
-	handUp: false,
-	adminPeekingIntoSession: "",
-	"session-key": "a7d6e589-f0f9-4290-943a-4f90652ea6df",
-	creationKey: "5908c8",
-	isPermalinkSession: true,
-	"hangout-url": "https://hangouts.google.com/hangouts/_/w4axbux73vcmlixumxrk5hlrcye",
-});
-db.sessions.push({
-	id: 2,
-	title: "Alternate breakout #2",
-	description: "Click the button below to join the breakout",
-	shortCode: "alt-session-2",
-	joinCap: 10,
-	handUp: false,
-	adminPeekingIntoSession: "",
-	"session-key": "4676dede-49a2-4c92-b94e-7fc0041d7d58",
-	creationKey: "b74271",
-	isPermalinkSession: true,
-	"hangout-url": "https://hangouts.google.com/hangouts/_/vrwapbspangf7oeoga7gta27vee",
-});
 
 var completedTasks = {
    setDisplayName: 1496442154,
